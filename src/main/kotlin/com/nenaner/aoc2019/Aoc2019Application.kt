9c1f69ffb1679fc54945de9c877cb2ae34e97cfd
Andy Ryan <addictedtobsg@gmail.com>
2019-12-02 19:43:14 -0500

    Initial commit of repo (day 1)

---
+++ src/main/kotlin/com/nenaner/aoc2019/Aoc2019Application.kt
@@ -0,0 +1,11 @@
+package com.nenaner.aoc2019
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Aoc2019Application
+
+fun main(args: Array<String>) {
+	runApplication<Aoc2019Application>(*args)
+}
---
+++ src/test/kotlin/com/nenaner/aoc2019/Aoc2019ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nenaner.aoc2019
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Aoc2019ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
